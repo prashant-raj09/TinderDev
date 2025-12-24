98e2f5226df7483528487e16a238a2529e2eab63
Prashant Raj <prashant-raj09@users.noreply.github.com>
2025-12-24 16:50:11 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -6,13 +6,45 @@ const User = require("./models/user");
 // Middleware -> From Postman we get data inthe form of JSON so this middleware convert the JSON in JS object
 app.use(express.json());
 
-app.post("/singup", async (req, res) => {
-  const user = new User(req.body)
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added succesfully..." + user);
   } catch (err) {
     res.status(400).send("Error occured" + err.message);
+  }
+});
+
+
+
+app.get("/signup", (req, res) => {
+  res.send("Data recived....");
+});
+
+// Finding user by emailId
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.send("User not found...");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(404).send("Something Went Worng....");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error recived" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,60 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      maxLength: 50,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      maxLength: 50,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      maxLength: 50,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      maxLength: 50,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid...");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is deafult about section....",
+    },
+    profile: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/12226/12226076.png",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
